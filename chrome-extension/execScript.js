ab98ee73191f092da05706e77839e3aa86f8efec
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-02-01 09:05:16 +0900

    [Clean] execScript.jsのリファクタリング

--- chrome-extension/execScript.js
+++ chrome-extension/execScript.js
@@ -1,12 +1,12 @@
 
 // 「windowオブジェクト・flashのメソッド」にアクセスする場合これが必要
 function execScript(fn) {
-	var script_text = typeof fn === "function" ? ("(" + fn.toString() + ")()") : fn;
+	const script_text = typeof fn === "function" ? `(${fn.toString()})()` : fn;
 	
-	var script = document.createElement('script');
+	const script = document.createElement("script");
 	script.appendChild(document.createTextNode(script_text));
 
-	var parent = (document.head || document.body || document.documentElement);
+	const parent = document.head || document.body || document.documentElement;
 	parent.appendChild(script);
 	parent.removeChild(script);
 }
